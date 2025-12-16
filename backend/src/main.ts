17d8a52fbdd78519dd3e1fb2a680934bfe06de72
RuslanIsHere <gabitovrv@outlook.com>
2025-12-16 17:52:52 +0100

    backend auth test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // выкидывает лишние поля из body
+        forbidNonWhitelisted: true, // если прислали лишнее поле — 400
+        transform: true, // приводит типы (полезно для query/params)
+      }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
